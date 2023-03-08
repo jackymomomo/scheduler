16c25633416af1bb1fcbe78031e7aa07e2a48626
jackymomomo <jacklloydrobinson@gmail.com>
2023-03-08 15:48:35 -0800

    classNames are now being used

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,15 +4,10 @@ import "components/Button.scss";
 const classNames = require('classnames')
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-     buttonClass += " button--danger";
-   }
+   let buttonClass = classNames('button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   });
 
    return (
      <button
