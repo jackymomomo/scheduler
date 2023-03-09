43812b2b38d90429bc8f7dd4a0f6276d70502001
jackymomomo <jacklloydrobinson@gmail.com>
2023-03-09 02:24:15 -0800

    minor bug fix, use Promis.all to run promised

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import DayListItem from 'components/dayListItem';
-
 // DayList renders a list of DayListItem components
 export default function DayList(props) {
-  const schedule = props.days.map(dayObj => {   
-     return (
+  const schedule = props.days.map(dayObj => {
+    return (
       <DayListItem
         key={dayObj.id}
-        name={dayObj.name}
-        spots={dayObj.spots}
-        selected={dayObj.name === props.day}
-        setDay={props.setDay} />         
+        selected={dayObj.name === props.day} 
+        {...dayObj}       
+      />
     )
   })
   return <ul>{schedule}</ul>
 }
-
-// The DayList Component Props: 
-// days:Array a list of day objects (each object includes an id, name, and spots)
