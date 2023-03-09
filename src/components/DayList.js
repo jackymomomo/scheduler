0a198d8699ca7e22d37d5d60a5ea34df0c69518d
jackymomomo <jacklloydrobinson@gmail.com>
2023-03-08 16:11:40 -0800

    Daylist compenent added + stories

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import DayListItem from 'components/dayListItem';
+
+// DayList renders a list of DayListItem components
+export default function DayList(props) {
+  const schedule = props.days.map(dayObj => {  
+    return (
+      <DayListItem
+        key={dayObj.id}
+        name={dayObj.name}
+        spots={dayObj.spots}
+        selected={dayObj.name === props.day}
+        setDay={props.setDay} />         
+    )
+  })
+  return <ul>{schedule}</ul>
+}
+
+// The DayList Component Props: 
+// days:Array a list of day objects (each object includes an id, name, and spots)
--- src/components/dayListItem.js
+++ src/components/dayListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "components/DayListItem.scss";
+import "components/dayListItem.scss";
 const classNames = require('classnames');
 
 const formatSpots = spots => {
